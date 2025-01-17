934f4e315870d39c89a5e672aa1ce1f2931570fb
Phil Underwood <beardydoc@gmail.com>
2025-01-17 18:12:47 +0000

    various bug fixes - now working!

--- examples/ros_adapter_simpletest.py
+++ examples/ros_adapter_simpletest.py
@@ -2,3 +2,22 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 Phil Underwood for Underwood Underground
 #
 # SPDX-License-Identifier: Unlicense
+import sys
+sys.path.insert(0,'..')
+import board
+import icm
+import time
+import digitalio
+import busio
+from ros_adapter import ROS_Adapter
+
+
+en_pin = digitalio.DigitalInOut(board.D4)
+en_pin.switch_to_output(False)
+i2c_bus = busio.I2C(board.SCL, board.SDA, frequency=400_000)
+device = icm.ICM20948(i2c_bus)
+print("created device")
+ros = ROS_Adapter.create_from_sensor(device, frame_id="map")
+print(ros.msgs)
+ros.run()
+print("exiting")
--- ros_adapter.py
+++ ros_adapter.py
@@ -83,40 +83,56 @@ if ROS_VERSION in (1,2):
             self.source_names = ["gyro", "acceleration"]
 
         def assign(self, source):
+            obj = self.cls()
             if hasattr(source, "gyro"):
-                g = Imu.angular_velocity
+                g = obj.angular_velocity
                 g.x, g.y, g.z = source.gyro
             if hasattr(source, "acceleration"):
-                a = Imu.linear_acceleration
+                a = obj.linear_acceleration
                 a.x, a.y, a.z = source.acceleration
+            return obj
+
+    class MagneticConverter(Converter):
+        def __init__(self):
+            super().__init__(MagneticField, "magnetic_field", "magnetic", scale=1 / 1_000_000)
+            
+        def assign(self, source):
+            obj = self.cls()
+            m = obj.magnetic_field
+            m.x, m.y, m.z = [x*self.scale for x in source.magnetic]
+            return obj
+    
 
     SENSOR_TYPES: List[Converter] = [
         Converter(FluidPressure, "fluid_pressure", "pressure", scale=0.01), # hectoPascals: Pascals
         Converter(Illuminance, "lux"), # lux: lux
         IMUConverter(), # rad/s : rad/s
-        Converter(MagneticField, "magnetic_field", "magnetic", scale=1 / 1_000_000), # microTesla: Tesla
+        MagneticConverter(),
         Converter(RelativeHumidity, "relative_humidity", scale=0.01), # percent: 0.0 .. 1.0
         Converter(Temperature, "temperature"), # celsius: celsius
         Converter(Range, "range", "distance", 0.01), # cm: m
-        ColorRGBA(), # RGB 8bit
+        ColorConverter(), # RGB 8bit
     ]
 
 class ROS_Adapter:
-    def __init__(self, object, rate:float=0.1, name: str = None):
+    def __init__(self, device, rate:float=0.1, name: str = None, frame_id: str = None):
         self.name = name
         self.rate = rate
+        self.device = device
+        self.frame_id = frame_id
         self.msgs: Dict[Converter, Publisher] = {}
         for cvt in SENSOR_TYPES:
-            if any(hasattr(object, attr) for attr in cvt.source_names):
+            if any(hasattr(device, attr) for attr in cvt.source_names):
                 self.msgs[cvt] = self._create_publisher(cvt)
 
     def _create_publisher(self, cvt:Converter):
         # this should be overridden
         raise NotImplementedError
 
-    def _publish_messages(self):
+    def _publish_messages(self, event=None):
         for cvt, pub in self.msgs.items():
-            msg = cvt.assign(self)
+            msg = cvt.assign(self.device)
+            msg.header.frame_id = self.frame_id
             pub.publish(msg)
 
     def run(self):
@@ -124,19 +140,19 @@ class ROS_Adapter:
         raise NotImplementedError
 
     @classmethod
-    def create_from_sensor(cls, object):
+    def create_from_sensor(cls, object, rate: float=0.1, name: str = None, frame_id:str = "base_link"):
         if ROS_VERSION==1:
-            return ROS1_Adapter(object)
+            return ROS1_Adapter(object, rate, name, frame_id)
         elif ROS_VERSION==2:
-            return ROS2_Adapter(object)
+            return ROS2_Adapter(object, rate, name, frame_id)
 
 class ROS1_Adapter(ROS_Adapter):
 
-    def __init__(self, object, rate: float = 0.1, name: str = None):
+    def __init__(self, object, rate: float = 0.1, name: str = None, frame_id:str = "base_link"):
         if name is None:
             name = object.__class__.__name__
         rospy.init_node(name)
-        super().__init__(object, rate, name)
+        super().__init__(object, rate, name, frame_id)
         period = Duration(0,rate*1_000_000_000)
         self.timer = rospy.timer.Timer(period, self._publish_messages)
 
@@ -147,11 +163,11 @@ class ROS1_Adapter(ROS_Adapter):
         rospy.spin()
 
 class ROS2_Adapter(ROS_Adapter):
-    def __init__(self, object, rate: float = 0.1, name: str = None):
+    def __init__(self, object, rate: float = 0.1, name: str = None, frame_id:str = "base_link"):
         if name is None:
             name = object.__class__.__name__
         self.node = rclpy.create_node(name)
-        super().__init__(object, rate, name)
+        super().__init__(object, rate, name, frame_id)
         self.timer = self.node.create_timer(rate,self._publish_messages)
 
     def _create_publisher(self, cvt: Converter):
